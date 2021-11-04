1767210a20354364161a4aaf25e22cdca9c3f037
Branny <brandonsidebottom@outlook.com>
2021-11-04 18:46:05 +0000

    List comprehensions complete

--- types/listcomprehension.py
+++ types/listcomprehension.py
@@ -28,6 +28,32 @@ print (l)
 # We can quickly perform expressions over members of lists
 # this is insane 
 
+# <expression> can be any valid python expression
+# this means listcomprehensions can be used
+# very useful for making matrix's
 
+m = [[j for j in range(5)] for i in range(6)]
+print(m)
+
+# flattening the matrix
+
+f = [value for sublist in m for value in sublist]
+print (f)
+
+# There are alternatives to comprehensions, like map() and reduce()
+# but screw that because these are beautiful
+
+# set comprehensions
+# use {} instead of []
+
+se = {s for s in range(1, 5) if s % 2}
+print(se)
+
+
+# Dictionary comprehension
+# requires a key and a value
+# key and value both defined in the expression
+d = { x: x**2 for x in (2, 4, 6)}
+print(d)
 
 
